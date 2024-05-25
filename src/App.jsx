4b6745158055ab1d0c2aad5742057201de829722
srutiii <srutidas0228@gmail.com>
2024-05-25 22:20:48 +0530

    add private route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+// App.js
 import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import HomePage from "./pages/HomePage";
-import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Chatbot from "./components/Chatbox";
@@ -10,23 +11,46 @@ import Appointments from "./pages/Appointments";
 import Articles from "./pages/Articles";
 import Profile from "./pages/Profile";
 import { ToastContainer } from "react-toastify";
- import "react-toastify/dist/ReactToastify.css";
+import "react-toastify/dist/ReactToastify.css";
+import { useAuth } from "./context/AuthContext";
 
 function App() {
+  const { loggedIn } = useAuth();
+
+  
+
   return (
     <>
       <NavBar />
       <Chatbot />
       <Routes>
-        <Route path="/" exact element={<HomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/predict" element={<PredictDisease />} />
-        <Route path="/book" element={<Appointments />} />
-        <Route path="/article" element={<Articles />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/login"
+          element={loggedIn ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/signup"
+          element={loggedIn ? <Navigate to="/" /> : <Signup />}
+        />
+        <Route
+          path="/predict"
+          element={loggedIn ? <PredictDisease /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/book"
+          element={loggedIn ? <Appointments /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/article"
+          element={loggedIn ? <Articles /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/profile"
+          element={loggedIn ? <Profile /> : <Navigate to="/login" />}
+        />
       </Routes>
-      <ToastContainer/>
+      <ToastContainer />
     </>
   );
 }
